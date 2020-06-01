72ed4f92109e3388ea72caefbabf1c4b9edb5df9
Damon Tian <dtian01@nyit.edu>
2020-06-01 15:18:18 -0400

    06.01.20: testing dynamic data 1

---
+++ src/pages/page3.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import axios from "axios"
+
+const Page2 = () => {
+
+  const [componentState, setComponentState] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("https://deliver.kontent.ai/71fb2b76-0e25-00af-1b74-6fa6edc2d73d/items/untitled_content_item_0b151c8")
+      .then(res => {
+        setComponentState(res.data.item);
+        setLoading(false);
+      })
+  }, [])
+
+
+  return (
+    <div className="container mx-auto">
+      {componentState && componentState.elements && (
+        <div className={`${loading ? "" : "fade-in"}`}>
+          <section className="section__b mb-16 gap-10 grid grid-cols-2 mb-16 section__b">
+            <div dangerouslySetInnerHTML={{ __html: componentState.elements.section__b__content1.value }}></div>
+          </section>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Page2

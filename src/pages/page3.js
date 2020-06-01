2b7f9094c4c68a851b940fbc783e9382cfca3882
Damon Tian <dtian01@nyit.edu>
2020-06-01 15:21:03 -0400

    06.01.20: testing dynamic data 2

--- src/pages/page3.js
+++ src/pages/page3.js
@@ -5,14 +5,14 @@ import axios from "axios"
 const Page2 = () => {
 
   const [componentState, setComponentState] = useState({});
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios
       .get("https://deliver.kontent.ai/71fb2b76-0e25-00af-1b74-6fa6edc2d73d/items/untitled_content_item_0b151c8")
       .then(res => {
         setComponentState(res.data.item);
-        setLoading(false);
+        // setLoading(false);
       })
   }, [])
 
@@ -20,7 +20,7 @@ const Page2 = () => {
   return (
     <div className="container mx-auto">
       {componentState && componentState.elements && (
-        <div className={`${loading ? "" : "fade-in"}`}>
+        <div>
           <section className="section__b mb-16 gap-10 grid grid-cols-2 mb-16 section__b">
             <div dangerouslySetInnerHTML={{ __html: componentState.elements.section__b__content1.value }}></div>
           </section>
